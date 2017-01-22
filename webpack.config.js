caedaccd59eceaf8c2b1c9e72a242a04d0f876aa
wangzengdi <sunnyadi@163.com>
2017-01-22 17:41:53 +0800

    save .eslintrc.json

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,34 @@
+// http://jamesknelson.com/webpack-made-simple-build-es6-less-with-autorefresh-in-26-lines/
+
 const path = require('path');
 const config = require('./config');
 const HtmlwebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: {
-    index: require.resolve('./src/examples/index'),
-    createMap: require.resolve('./src/map/createMap'),
-  },
+  entry: [
+    'babel-polyfill',
+    {
+      index: require.resolve('./src/examples/index'),
+      createMap: require.resolve('./src/map/createMap'),
+    },
+  ],
   output: {
     path: config.outputdir,
     publicPath: config.publicPath,
     filename: '[name].bundle.js',
+  },
+  devtool: 'source-map',
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        include: path.join(config.basedir, 'src'),
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015'],
+        },
+      },
+    ],
   },
   plugins: [
     new HtmlwebpackPlugin(),
